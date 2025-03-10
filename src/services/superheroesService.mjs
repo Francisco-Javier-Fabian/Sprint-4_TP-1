b1ee0cb846bb06b6d5a444140dd2c2c00b15e7e7
Francisco Javier Fabian <milan.kta@gmail.com>
2025-03-10 17:00:23 -0300

    Agregado: capa services

---
+++ src/services/superheroesService.mjs
@@ -0,0 +1,18 @@
+import SuperHeroRepository from "../repositories/SuperHeroRepository.mjs";
+
+export async function obternerSuperheroePorId(id) {
+    return await SuperHeroRepository.obtenerPorId(id);
+}
+
+export async function obtenerTodosLosSuperheroes() {
+    return await SuperHeroRepository.obtenerTodos();
+}
+
+export async function buscarSuperheroesPorAtributo(atributo, valor) {
+    return await SuperHeroRepository.buscarPorAtributo(atributo, valor);
+}
+
+export async function obtenerSuperheroesMayoresDe30() {
+    return await SuperHeroRepository.obtenerMayoresDe30();
+}
+//cargDO de la logica de negocios valida que los datos tengan el tipo de dato nesesario en caso contrario lo transforma
