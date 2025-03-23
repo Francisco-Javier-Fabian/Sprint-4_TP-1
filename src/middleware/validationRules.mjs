58b0ee51b84c2b78a31748d596acdd1cd14e2c90
Francisco Javier Fabian <milan.kta@gmail.com>
2025-03-22 21:02:45 -0300

    pruebas finalizadas y testeadas

--- src/middleware/validationRules.mjs
+++ src/middleware/validationRules.mjs
@@ -10,11 +10,14 @@ import { body } from 'express-validator';
 
 export const validationDataSuperHeros = () => [
     body('nombreSuperHeroe').notEmpty().withMessage('Nombre del superheroe es necesario')
-    .isLength({ min: 3, max: 60 }).trim(),
+    .isLength({ min: 3, max: 60 }).withMessage('El nombre superheoristico debe tener entre 3 y 60 caracteres per favore').trim(),
     body('nombreReal').notEmpty().withMessage('Nombre Real es requerido')
-    .isLength({ min: 3, max: 60 }).trim(),
-    body('edad').notEmpty().withMessage('Nombre Real es requerido')
-    .isInt({ min: 0 }).trim(),
+    .isLength({ min: 3, max: 60 }).withMessage('El nombre real debe tener entre 3 y 60 caracteres per favore').trim(),
+    body('edad').notEmpty().withMessage('La edad es requerido')
+    .isInt({ min: 0 }).withMessage('Edad es incorrecta').trim(),
     body('poderes').notEmpty().withMessage('Lista de poderes requerida gil')
-    .isArray({ min: 1 }).isLength({ min: 3, max: 60}).trim(),
+    .isArray({ min: 1 }).withMessage('Poderes no es un array o esta vacio'),
+    body('poderes.*')
+    .isString().withMessage('Cada poder debe tener una cadena de texto')
+    .isLength({ min: 3, max: 60 }).withMessage('Cada poder debe tener entre 3 y 60 caracteres per favore').trim()
 ]
--- src/routes/superHeroRoutes.mjs
+++ src/routes/superHeroRoutes.mjs
@@ -11,18 +11,22 @@ import {
     eliminarSuperheroePorIdController, 
     eliminarSuperheroePorNombreController
 } from '../controllers/superheroesController.mjs';
+import { validationDataSuperHeros } from '../middleware/validationRules.mjs';
+import { handleValidationErrors } from '../middleware/errorMiddleware.mjs';
 
 const router = express.Router();
+
 
 router.get('/heroes', obtenerTodosLosSuperheroesController);
 router.get('/heroes/:id', obtenerSuperheroePorIdController);
 router.get('/heroes/buscar/:atributo/:valor', buscarSuperheroesPorAtributoController);
 router.get('/heroes/buscar/mayores-30', obtenerSuperheroesMayoresDe30Controller);
 // Sprint 3 
-router.post('/heroes/crear', crearNuevoSuperheroeController);
+router.post('/heroes/crear', validationDataSuperHeros(), handleValidationErrors, crearNuevoSuperheroeController);
 // Sprint 3 
- router.put('/heroes/actualizar/:id', actualizarSuperheroeController); 
+ router.put('/heroes/actualizar/:id', validationDataSuperHeros(), handleValidationErrors,  actualizarSuperheroeController); 
  router.delete('/heroes/eliminar/id/:id', eliminarSuperheroePorIdController); 
  router.delete('/heroes/eliminar/nombre/:nombre', eliminarSuperheroePorNombreController); 
 // Se cambia la ruta de heroes por que al ser la busqueda por cascada toma el id y no llega a mayores-30
+//
 export default router;
